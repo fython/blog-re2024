a537da795b5af9457e06e443ec911996874f8f0d
Siubeng <fython@163.com>
2024-07-13 15:27:31 +0800

    fix: incorrect time zone display

--- src/components/Datetime.tsx
+++ src/components/Datetime.tsx
@@ -54,11 +54,13 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
     year: "numeric",
     month: "short",
     day: "numeric",
+    timeZone: "Asia/Shanghai",
   });
 
   const time = myDatetime.toLocaleTimeString(LOCALE.langTag, {
     hour: "2-digit",
     minute: "2-digit",
+    timeZone: "Asia/Shanghai",
   });
 
   return (
@@ -66,7 +68,7 @@ const FormattedDatetime = ({ pubDatetime, modDatetime }: DatetimesProps) => {
       <time dateTime={myDatetime.toISOString()}>{date}</time>
       <span aria-hidden="true"> | </span>
       <span className="sr-only">&nbsp;at&nbsp;</span>
-      <span className="text-nowrap">{time}</span>
+      <span className="text-nowrap">{time} @ UTC +8</span>
     </>
   );
 };
