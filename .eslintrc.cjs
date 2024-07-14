ff20ee36f4f10a931c4ff18973792b3a30afaccf
Siubeng <fython@163.com>
2024-07-14 17:04:36 +0800

    feat: support comments (powered by giscus) & fix eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,10 +4,13 @@ module.exports = {
     es2022: true,
     browser: true,
   },
-  extends: ["eslint:recommended", "plugin:astro/recommended"],
+  extends: ["eslint:recommended", "plugin:react/recommended", "plugin:astro/recommended"],
   parserOptions: {
     ecmaVersion: "latest",
     sourceType: "module",
+    ecmaFeatures: {
+      jsx: true,
+    },
   },
   overrides: [
     {
