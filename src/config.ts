5a224f5b1e19e73e91c959948354c060cf17fbd4
Siubeng <fython@163.com>
2024-07-15 08:23:30 +0000

    feat: transfer old article & change posts per page

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ export const SITE: Site = {
   title: "烧饼吹水铺",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
-  postPerPage: 3,
+  postPerPage: 5,
   scheduledPostMargin: 15 * 60 * 1000, // 15 minutes
 };
 
