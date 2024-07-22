8c428dae8be3523fd8442137774eee98ae17a937
Siubeng <fython@163.com>
2024-07-22 09:24:11 +0000

    fix chinese font rendering in generated og images

--- src/utils/generateOgImages.tsx
+++ src/utils/generateOgImages.tsx
@@ -3,20 +3,15 @@ import { Resvg } from "@resvg/resvg-js";
 import { type CollectionEntry } from "astro:content";
 import postOgImage from "./og-templates/post";
 import siteOgImage from "./og-templates/site";
+import * as fs from "fs/promises";
 
 const fetchFonts = async () => {
-  // Regular Font
-  const fontFileRegular = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.regular.ttf"
+  const fontRegular: ArrayBuffer = await fs.readFile(
+    "./fonts/SourceHanSerifSC-Regular.otf"
   );
-  const fontRegular: ArrayBuffer = await fontFileRegular.arrayBuffer();
-
-  // Bold Font
-  const fontFileBold = await fetch(
-    "https://www.1001fonts.com/download/font/ibm-plex-mono.bold.ttf"
+  const fontBold: ArrayBuffer = await fs.readFile(
+    "./fonts/SourceHanSerifSC-Bold.otf"
   );
-  const fontBold: ArrayBuffer = await fontFileBold.arrayBuffer();
-
   return { fontRegular, fontBold };
 };
 
@@ -28,13 +23,13 @@ const options: SatoriOptions = {
   embedFont: true,
   fonts: [
     {
-      name: "IBM Plex Mono",
+      name: "Source Han Serif SC",
       data: fontRegular,
       weight: 400,
       style: "normal",
     },
     {
-      name: "IBM Plex Mono",
+      name: "Source Han Serif SC",
       data: fontBold,
       weight: 600,
       style: "normal",
