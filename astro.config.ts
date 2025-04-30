5039b97342498653e075f9d2f3600070b41ed0b8
Siubeng <fython@163.com>
2025-04-30 08:56:04 +0000

    fix: remove unsupported config for astro latest

--- astro.config.ts
+++ astro.config.ts
@@ -35,11 +35,6 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
-  image: {
-    // Used for all Markdown images; not configurable per-image
-    // Used for all `<Image />` and `<Picture />` components unless overridden with a prop
-    experimentalLayout: "responsive",
-  },
   experimental: {
     svg: true,
     responsiveImages: true,
